438e8dc3185f77f1a22e19e5ae0ddd26d0c16d1d
root <root@localhost.localdomain>
2023-05-05 16:31:21 +0100

    upload new project

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify, render_template
+
+# Create the app object
+app = Flask(__name__)
+
+
+# importing function for calculations
+from basic_signe_function import basic_signe
+
+# Define calculator
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    a = request.form['a']
+    
+
+    result = basic_signe(a)
+
+    return render_template('index.html', prediction_text=str(result))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=82)
+
---
+++ basic_signe_function.py
@@ -0,0 +1,19 @@
+def basic_signe(a):
+
+  if (a.isnumeric()):
+    a=float(a)
+    
+    if a>0 :
+      result = "positive number "
+    elif a == 0:
+      result = "null  number"
+    elif a < 0:
+      result = "negative number"
+   
+   
+    
+   else:
+     result = "Please enter a valid number "
+    
+
+  return result
