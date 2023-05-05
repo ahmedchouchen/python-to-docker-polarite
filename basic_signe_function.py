8cf00e21c50e17eaa6b6edbcba8feea7c4efa694
ahmedchouchen <93875302+ahmedchouchen@users.noreply.github.com>
2023-05-05 18:26:01 +0200

    Update basic_signe_function.py

--- basic_signe_function.py
+++ basic_signe_function.py
@@ -1,6 +1,6 @@
 def basic_signe(a):
 
-<<<<<<< HEAD
+
     if (a.isnumeric()):
       a=float(a)
     
@@ -18,16 +18,3 @@ def basic_signe(a):
     
 
     return result
-=======
-  if (a.isnumeric()):
-     a=float(a)    
-      if a>0 :
-        result = "positive number "
-      elif a == 0:
-        result = "null  number"
-      else :
-        result = "negative number"    
-   else:
-     result = "Please enter a valid number "
-return result
->>>>>>> 4538e95d431236e568315b95ca6addff53effef0
